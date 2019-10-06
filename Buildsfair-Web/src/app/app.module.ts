3da95a0371bce692a987519e42c8375f6853cb2c
noenemy <noenemy@gmail.com>
2019-10-06 22:18:00 +0900

    Implement StageObject service

--- Buildsfair-Web/src/app/app.module.ts
+++ Buildsfair-Web/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { StagelogService } from './_services/stagelog.service';
 import { LeaderboardService } from './_services/leaderboard.service';
 import { HttpClientModule } from '@angular/common/http';
 import { StageService } from './_services/stage.service';
+import { StageObjectService } from './_services/stageobject.service';
 
 @NgModule({
    declarations: [
@@ -34,7 +35,8 @@ import { StageService } from './_services/stage.service';
       GameService,
       StagelogService,
       LeaderboardService,
-      StageService
+      StageService,
+      StageObjectService
    ],
    bootstrap: [
       AppComponent
