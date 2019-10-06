cb28a6ef91595c434dcf009056af34406ae56a6e
noenemy <noenemy@gmail.com>
2019-10-06 14:52:01 +0900

    Implement leaderboard component

--- Buildsfair-Web/src/app/leaderboard/leaderboard.component.ts
+++ Buildsfair-Web/src/app/leaderboard/leaderboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Ranking } from '../_models/ranking';
+import { LeaderboardService } from '../_services/leaderboard.service';
 
 @Component({
   selector: 'app-leaderboard',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leaderboard.component.css']
 })
 export class LeaderboardComponent implements OnInit {
+  rankingColumns: string[];
+  rankings: Ranking[];
 
-  constructor() { }
+  constructor(private http: HttpClient,
+              private leaderboardService: LeaderboardService) { }
 
   ngOnInit() {
+    this.rankingColumns = this.getRankingColumns();
+    this.getRankings();
   }
 
+  getRankings() {
+    this.leaderboardService.getRankings().subscribe((rankings: Ranking[]) => {
+      this.rankings = rankings;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getRankingColumns(): string[] {
+    return ['total_rank', 'game_id', 'name', 'total_score', 'total_found_objects', 'total_playtime'];
+  }
 }
