bd14af87dce07f1472ae066ff807859ddebc07b1
noenemy <noenemy@gmail.com>
2019-10-06 13:34:57 +0900

    Migrated main navigation feature

--- Buildsfair-Web/src/app/app-routing.module.ts
+++ Buildsfair-Web/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { GameComponent } from './game/game.component';
+import { DebugComponent } from './debug/debug.component';
+import { TestComponent } from './test/test.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'leaderboard', component: LeaderboardComponent },
+  { path: 'game', component: GameComponent },
+  { path: 'debug', component: DebugComponent },
+  { path: 'playdemo', component: TestComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
--- Buildsfair-Web/src/app/app.module.ts
+++ Buildsfair-Web/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { DebugComponent } from './debug/debug.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      NavComponent
+      NavComponent,
+      HomeComponent,
+      GameComponent,
+      LeaderboardComponent,
+      DebugComponent,
+      TestComponent
    ],
    imports: [
       BrowserModule,
