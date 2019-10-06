d6636289a07b09524303b29305c183a0f9ed87d1
noenemy <noenemy@gmail.com>
2019-10-06 14:05:02 +0900

    Migrated home component

--- Buildsfair-Web/src/app/home/home.component.ts
+++ Buildsfair-Web/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TweenMax } from 'node_modules/gsap';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.animate();
   }
 
+  animate(): void {
+    TweenMax.fromTo('h1', 3, {x: 250}, {x: 0});
+    TweenMax.fromTo('#p1', 2.4, {x: 300}, {x: 0});
+    TweenMax.fromTo('#p2', 3.3, {x: 300}, {x: 0});
+  }
+  playGame() {
+    this.router.navigateByUrl('/game');
+  }
+
+  playTrailer() {
+    this.router.navigateByUrl('/trailer');
+  }
 }
